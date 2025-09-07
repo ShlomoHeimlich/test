663425c0c112ff0d0b2872c31f0a40bec30be7f3
shlomo <s0556732899@gmail.com>
2025-09-07 17:57:27 +0300

    tyme

--- Client/src/CreatPost.tsx
+++ Client/src/CreatPost.tsx
@@ -1,16 +1,17 @@
-import "../src/style/CreatPost.css"
+import "../src/style/CreatPost.css";
 interface post {
   name: string;
   url_img: string;
   description: string;
 }
 export default function CreatPost(props: { post: post }) {
+    const formattedDateTime = new Date().toLocaleString();
   return (
     <div id="CreatPost">
-      <p>{props.post.name}</p> 
+      <p>{props.post.name}</p>
       <img src={props.post.url_img} alt="" />
-      <p>{new Date().getFullYear()}</p>
       <p>{props.post.description}</p>
+      <p>{formattedDateTime}</p>
       <p>num of liks</p>
     </div>
   );
