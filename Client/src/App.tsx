5f9bd0d965cf3547952760de9c21b1fca6172b08
shlomo <s0556732899@gmail.com>
2025-09-07 17:45:10 +0300

    posts

---
+++ Client/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Posts from "../src/posts.tsx";
+
+function App() {
+  return (
+    <>
+      <Posts></Posts>
+    </>
+  );
+}
+
+export default App;
